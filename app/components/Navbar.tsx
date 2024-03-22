d4445e7b82afd1a12a98ef0e5b382a94cbe8cc15
Jacob Mompean <jacob.mompean@alliantgroup.com>
2024-03-22 15:03:34 -0500

    move Nav to root layout

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from './ModeToggle';
 
 export default function Navbar() {
   return (
-    <nav className='w-full relative flex items-center justify-between'>
+    <nav className='w-full relative flex items-center justify-between p-4'>
       <Link href='/' className='font-bold text-3xl font-sans'>
         <span className='text-primary'>a</span>lliantgroup
         <span className='text-primary'>Blog</span>
