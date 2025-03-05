cadaded827a941a12d5094f3233f89600411bca7
Jacob Mompean <jacob.mompean@alliantgroup.com>
2025-03-05 16:29:22 -0600

    refactor: update RootLayout to suppress hydration warning and change theme attribute for improved compatibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider
-          attribute='class'
+          attribute='data-mode'
           defaultTheme='system'
           enableSystem
           disableTransitionOnChange
