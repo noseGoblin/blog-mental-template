88360416c6a8c809d4dcbb233bb300a59961e3e2
Jacob Mompean <jacob.mompean@alliantgroup.com>
2025-03-05 13:48:58 -0600

    feat: Enable React strict mode and update dependencies in package.json

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,6 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'v700z5ue',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
+  reactStrictMode: true,
 })
